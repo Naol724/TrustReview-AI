9fb40a8b2d5a089c1185a3bf3fbe33a7e1808f91
Naol Gonfa <naolgonfa449@gmail.com>
2026-04-20 16:36:20 +0300

    backend link is inserted into frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:5000'
+const API_BASE_URL = 'https://trustreview-ai-u53d.onrender.com/'
 
 export interface PredictionRequest {
   review: string
